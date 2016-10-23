b2ec420e924614b25650f8aecdc45c3eb8c38400
Sergio Vilar <sergiovilar.r@gmail.com>
2016-10-23 12:13:43 -0300

    Split in groups of 30 items
    Gmail doesn't accept a large query

--- main.rb
+++ main.rb
@@ -5,11 +5,13 @@ data = JSON.parse(File.read('./data.json'))
 entries = ''
 
 data['rules'].each do |rule|
-  rules = []
-  rules = rule['words'] unless rule['words'].nil?
-  rule['from'].each { |from| rules << "from:#{from}" } unless rule['from'].nil?
-  rule['subject'].each { |subject| rules << "subject:#{subject}" } unless rule['subject'].nil?
-  entries << ERB.new(File.read("./templates/#{rule['action']}.erb")).result(binding)
+  rows = []
+  rows = rule['words'] unless rule['words'].nil?
+  rule['from'].each { |from| rows << "from:#{from}" } unless rule['from'].nil?
+  rule['subject'].each { |subject| rows << "subject:#{subject}" } unless rule['subject'].nil?
+  rows.each_slice(30).to_a.each do |rules|
+    entries << ERB.new(File.read("./templates/#{rule['action']}.erb")).result(binding)
+  end
 end
 
 File.write('./filter.xml', ERB.new(File.read("./templates/main.erb")).result(binding))
